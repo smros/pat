1bd5fd6378a8a5ac697f4a38ef133a1f946552d6
steve <steve@steve-VirtualBox11.(none)>
2012-02-12 00:11:43 -0700

    Edits to state handler.

--- src/handlers/state/handler.py
+++ src/handlers/state/handler.py
@@ -9,16 +9,17 @@ import api
 
 # load config
 CFG_PATH = os.path.join(
-    os.path.dirname(__file__), '..','..', 'notify.cfg')
+    os.path.dirname(__file__), '..','..','..', 'notify.cfg')
 CONFIG = RawConfigParser()
 CONFIG.read(CFG_PATH)
 WATCHER_PATH = os.path.join(
-    os.path.dirname(__file__), '..',CONFIG.get('watcher', 'path') )
+    os.path.dirname(__file__), '..','..','..',CONFIG.get('watcher', 'path') )
 
 class StateHandler():
     
-    def __init__(self):
-        pass
+    def __init__(self,interested_in, getrecipients):
+        self.interested_in = interested_in
+        self.getrecipients = getrecipients
         
     def handle(self, message):
         print message
